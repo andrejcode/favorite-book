bb7db074ba9e737c3b250ad06dd75c6a52abf70e
Andrej <amilanovi0@gmail.com>
2022-07-16 18:03:41 +0200

    Provide room database

--- app/src/main/java/com/andrejmilanovic/favoritebook/di/AppModule.kt
+++ app/src/main/java/com/andrejmilanovic/favoritebook/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.andrejmilanovic.favoritebook.di
 
+import android.content.Context
+import androidx.room.Room
+import com.andrejmilanovic.favoritebook.data.local.BookDao
+import com.andrejmilanovic.favoritebook.data.local.BookDatabase
 import com.andrejmilanovic.favoritebook.data.remote.BookApi
 import com.andrejmilanovic.favoritebook.data.repository.BookRepository
 import com.andrejmilanovic.favoritebook.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -26,5 +31,14 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideBookRepository(api: BookApi) = BookRepository(api)
+    fun provideDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, BookDatabase::class.java, "book_database").build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: BookDatabase) = database.bookDao()
+
+    @Singleton
+    @Provides
+    fun provideBookRepository(api: BookApi, dao: BookDao) = BookRepository(api, dao)
 }
